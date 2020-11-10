2e4b929b609b52ed84ca0baefeab03675b95a789
Nicolas Bovatick <fariasnico16@gmail.com>
2020-11-10 18:35:25 -0300

    react first

---
+++ client/src/NoteForm.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+const NoteForm = ({ addNote }) => {
+
+  // state hook para el form
+  const [title, setTitle] = useState('');
+  const [text, setText] = useState('');
+
+  // handlers
+  const handleSubmit = e => {
+    e.preventDefault();
+    addNote({
+      title: title,
+      text: text
+    });
+    // blanquear formulario
+    setTitle('');
+    setText('');
+  };
+
+  // render JSX
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label htmlFor="title">Título</label>
+        <input
+          id="title"
+          className="form-control"
+          type='text'
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="text">Texto</label>
+        <textarea
+          id="text"
+          className="form-control"
+          value={text}
+          rows="4"
+          onChange={e => setText(e.target.value)}
+        >
+        </textarea>
+      </div>
+      <input
+        className="btn btn-primary"
+        type="submit"
+        value="Guardar"
+      />
+    </form>
+  );
+};
+
+export default NoteForm;
